2060cb3d73f0c50b663461066d980dd9749d812a
Kensya <himemusou@gmail.com>
2023-10-17 11:03:18 +0300

    React 3.3.6 button fix

--- src/components/FiltersBtns/FiltersBtns.js
+++ src/components/FiltersBtns/FiltersBtns.js
@@ -1,23 +1,22 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import classNames from 'classnames';
 
 import style from './FiltersBtns.module.scss';
 
 export default function FiltersBtns() {
   const possibleOptions = ['Самый дешевый', 'Самый быстрый', 'Оптимальный'];
-  const defaultCheckedBtn = ['Самый дешевый'];
+  const defaultCheckedBtn = 'Самый дешевый';
 
-  // const [activeBtn, setActiveBtn] = useState(defaultCheckedBtn);
+  const [activeBtn, setActiveBtn] = useState(defaultCheckedBtn);
 
   const filters = (filtersList) => {
     return filtersList.map((filterBtn) => {
       const btnClass = classNames(style['content__filterBtn'], {
-        [style.active]: filterBtn === defaultCheckedBtn[0],
+        [style.active]: filterBtn === activeBtn,
       });
-      console.log();
 
       return (
-        <button className={btnClass} key={filterBtn}>
+        <button className={btnClass} key={filterBtn} onClick={() => setActiveBtn(filterBtn)}>
           {filterBtn}
         </button>
       );
